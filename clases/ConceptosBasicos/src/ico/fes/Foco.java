01d8c7809ca88874d80f2083599597b9cc4e98a3
Undeck <omaravendanogaribay@hotmail.com>
2022-02-24 12:40:30 -0600

    iniciamos el archivo foco.java

---
+++ clases/ConceptosBasicos/src/ico/fes/Foco.java
@@ -0,0 +1,44 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ico.fes;
+
+/**
+ *
+ * @author OmarA
+ */
+public class Foco {
+    private String color;
+    private String tipo;
+    private int tamano;
+
+    public Foco() {
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public int getTamano() {
+        return tamano;
+    }
+
+    public void setTamano(int tamano) {
+        this.tamano = tamano;
+    }
+    
+    
+}
