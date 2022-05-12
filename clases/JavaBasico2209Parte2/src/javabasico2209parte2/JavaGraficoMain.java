0e2f5903acd875765f977f7b7204761929a1fa6a
Undeck <omaravendanogaribay@hotmail.com>
2022-05-12 13:03:05 -0500

    Codigo del 12 de Mayo

--- clases/JavaBasico2209Parte2/src/javabasico2209parte2/JavaGraficoMain.java
+++ clases/JavaBasico2209Parte2/src/javabasico2209parte2/JavaGraficoMain.java
@@ -3,8 +3,9 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package javabasico2209parte2;
-
+import ico.fes.awt.Ventana;
 import ico.fes.awt.VentanaVersion2;
+import ico.fes.swing.VentanaSwing;
 
 /**
  *
@@ -15,8 +16,8 @@ public class JavaGraficoMain {
      public static void main(String[] args) {
         //Ventana ventana = new Ventana("Hola AWT ICO Fes Aragón");
         
-         VentanaVersion2 ventana= new VentanaVersion2();
-        
+         VentanaSwing vs=new VentanaSwing();
+        System.out.println( System.getProperty("user.dir"));
     }
     
 }
